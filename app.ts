cdde04f7b2de005d63db89688b3e0bad362b22dc
okanay <okanay@hotmail.com>
2024-11-22 20:01:19 +0300

    v25

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/serve-static";
 import fs from "fs";
+import crypto from "crypto";
+import { exec } from "child_process";
 
 const app = new Hono();
 app.use(
@@ -27,6 +29,51 @@ app.get("/", (c) => {
   const html = Bun.file("./dist/main/index.html");
   return c.html(html.text());
 });
+
+const secret = "LHOUwjBFGyori7tltMnRQ2YtanvObPZOenCowk/Cq8c=";
+app.post("/github-push-event", async (c) => {
+  const githubEvent = c.req.header("X-GitHub-Event");
+  const signature = c.req.header("X-Hub-Signature-256");
+
+  if (githubEvent !== "push" || !signature) {
+    return c.json({ error: "Invalid event or missing signature" }, 400);
+  }
+
+  const payload = await c.req.text();
+  const hmac = crypto.createHmac("sha256", secret);
+  hmac.update(payload);
+  const expectedSignature = `sha256=${hmac.digest("hex")}`;
+
+  if (signature !== expectedSignature) {
+    return c.json({ error: "Invalid signature" }, 401);
+  }
+
+  // Hemen yanıt gönder
+  c.status(200);
+  c.json({ message: "Received" });
+
+  // Build process'i ayrı bir işlemde başlat
+  setTimeout(() => {
+    runBuildProcess();
+  }, 0);
+
+  return;
+});
+
+function runBuildProcess(): void {
+  exec(
+    "sudo systemctl restart mdb-menuarts.service",
+    { cwd: "/root/mdb-code" },
+    (error: Error | null, stdout: string, stderr: string) => {
+      if (error) {
+        console.error(`Exec error: ${error}`);
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`Command output: ${stdout}`);
+    },
+  );
+}
 
 const port = 3080;
 console.log(`Server is running on port ${port}`);
