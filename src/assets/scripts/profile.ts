aa0dccb16f6725f0599a4441594353843b0551d2
okanay <okanay@hotmail.com>
2024-11-30 12:39:28 +0300

    185

--- src/assets/scripts/profile.ts
+++ src/assets/scripts/profile.ts
@@ -8,7 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
         toggleElements: ['#profile-menu-button'],
         openElements: [],
         contentElement: '#profile-navigation',
-        closeElements: [],
+        closeElements: [
+          '#profile-info-1',
+          '#profile-info-2',
+          '#profile-info-3',
+          '#profile-info-4',
+          '#profile-info-5',
+        ],
         containers: ['#profile-navigation-content', '#profile-navigation'],
       },
     ],
@@ -38,6 +44,86 @@ document.addEventListener('DOMContentLoaded', () => {
             width: 'auto',
           },
         },
+      },
+    },
+  )
+
+  new ModalController(
+    [
+      {
+        id: 'reservation',
+        toggleElements: [],
+        openElements: ['#profile-info-1'],
+        contentElement: '#profile-content-1',
+        closeElements: [],
+        containers: ['#profile-content-1'],
+      },
+      {
+        id: 'favorites',
+        toggleElements: [],
+        openElements: ['#profile-info-2'],
+        contentElement: '#profile-content-2',
+        closeElements: [],
+        containers: ['#profile-content-2'],
+      },
+      {
+        id: 'reviews',
+        toggleElements: [],
+        openElements: ['#profile-info-3'],
+        contentElement: '#profile-content-3',
+        closeElements: [],
+        containers: ['#profile-content-3'],
+      },
+      {
+        id: 'password-email',
+        toggleElements: [],
+        openElements: ['#profile-info-4'],
+        contentElement: '#profile-content-4',
+        closeElements: [],
+        containers: ['#profile-content-4'],
+      },
+      {
+        id: 'preferences',
+        toggleElements: [],
+        openElements: ['#profile-info-5'],
+        contentElement: '#profile-content-5',
+        closeElements: [],
+        containers: ['#profile-content-5'],
+      },
+    ],
+    {
+      initialActiveModal: 'reservation',
+      outsideClickClose: false,
+      escapeClose: false,
+      preserveModalHistory: false,
+      attributes: {
+        stateAttribute: 'data-state',
+        values: {
+          open: 'open',
+          preserved: 'open',
+          hidden: 'closed',
+        },
+      },
+      scrollTo: {
+        enabled: true,
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'start',
+        offset: 160,
+      },
+      scrollLock: {
+        enabled: false,
+      },
+      urlState: {
+        enabled: true,
+        queryParam: 'view',
+        modals: [
+          'reservation',
+          'favorites',
+          'reviews',
+          'password-email',
+          'preferences',
+        ],
       },
     },
   )
