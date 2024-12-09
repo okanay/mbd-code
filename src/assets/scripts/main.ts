bc4ee6a29ef26948cb0bf36e499901ba8d77a29c
okanay <okanay@hotmail.com>
2024-12-09 12:46:46 +0300

    432

--- src/assets/scripts/main.ts
+++ src/assets/scripts/main.ts
@@ -29,6 +29,17 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   )
 
+  new Carousel('activity-list', 'activity-prev-btn', 'activity-next-btn', {
+    snapAlign: 'center',
+    itemSpacing: 16,
+    btnsDisableThreshold: 32,
+    screenSizes: [
+      { width: 1024, jumpVal: 3 },
+      { width: 768, jumpVal: 2 },
+      { width: 512, jumpVal: 1 },
+    ],
+  })
+
   const slider = new Slider({
     container: '#hero-slider-container',
     slideSelector: '.hero-slide',
