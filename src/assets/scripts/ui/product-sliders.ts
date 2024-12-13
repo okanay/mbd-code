4926a67832a97ad24e053bc2fe838232d5c5526f
okanay <okanay@hotmail.com>
2024-12-13 12:02:34 +0300

    454

--- src/assets/scripts/ui/product-sliders.ts
+++ src/assets/scripts/ui/product-sliders.ts
@@ -4,243 +4,7 @@ import { UpdateProductSliderDataItems } from '../packages/multi-group-gallery.js
 import { Slider } from '../packages/slider.js'
 import { TouchDirectionDetector } from '../packages/touch-event.js'
 
-document.addEventListener('DOMContentLoaded', () => {
-  // Product Slider Instance
-  const mainSlider = new Slider({
-    container: '#product-slider-container',
-    slideSelector: '.product-slide',
-    buttonSelector: '.product-slider-btn',
-    nextButtonSelector: '#slider-next',
-    prevButtonSelector: '#slider-prev',
-    defaultActiveIndex: 0,
-    activeButtonClass: 'product-slider-active-btn',
-    activeButtonClassTarget: '.product-slider-btn-item',
-    ...mainConfig,
-    ...config,
-  })
-
-  new Slider({
-    container: '#slider-container-1-desktop',
-    slideSelector: '.slide-1-desktop',
-    buttonSelector: '.slide-1-btn-desktop',
-    nextButtonSelector: '#slider-1-next-desktop',
-    prevButtonSelector: '#slider-1-prev-desktop',
-    defaultActiveIndex: 0,
-    activeButtonClass: 'product-slider-active-btn',
-    activeButtonClassTarget: '.slide-1-btn-desktop',
-    ...config,
-  })
-
-  new Slider({
-    container: '#slider-container-2-desktop',
-    slideSelector: '.slide-2-desktop',
-    buttonSelector: '.slide-2-btn-desktop',
-    nextButtonSelector: '#slider-2-next-desktop',
-    prevButtonSelector: '#slider-2-prev-desktop',
-    defaultActiveIndex: 0,
-    activeButtonClass: 'product-slider-active-btn',
-    activeButtonClassTarget: '.slide-2-btn-desktop',
-    ...config,
-  })
-
-  new Slider({
-    container: '#slider-container-3-desktop',
-    slideSelector: '.slide-3-desktop',
-    buttonSelector: '.slide-3-btn-desktop',
-    nextButtonSelector: '#slider-3-next-desktop',
-    prevButtonSelector: '#slider-3-prev-desktop',
-    defaultActiveIndex: 0,
-    activeButtonClass: 'product-slider-active-btn',
-    activeButtonClassTarget: '.slide-3-btn-desktop',
-    ...config,
-  })
-
-  new Slider({
-    container: '#slider-container-4-desktop',
-    slideSelector: '.slide-4-desktop',
-    buttonSelector: '.slide-4-btn-desktop',
-    nextButtonSelector: '#slider-4-next-desktop',
-    prevButtonSelector: '#slider-4-prev-desktop',
-    defaultActiveIndex: 0,
-    activeButtonClass: 'product-slider-active-btn',
-    activeButtonClassTarget: '.slide-4-btn-desktop',
-    ...config,
-  })
-
-  const mobileSlider1 = new Slider({
-    container: '#slider-container-1',
-    slideSelector: '.slide-1',
-    buttonSelector: '.slide-1-btn',
-    nextButtonSelector: '#slider-1-next',
-    prevButtonSelector: '#slider-1-prev',
-    defaultActiveIndex: 0,
-    activeButtonClass: 'product-slider-active-btn',
-    activeButtonClassTarget: '.slide-1-btn',
-    ...config,
-  })
-
-  const mobileSlider2 = new Slider({
-    container: '#slider-container-2',
-    slideSelector: '.slide-2',
-    buttonSelector: '.slide-2-btn',
-    nextButtonSelector: '#slider-2-next',
-    prevButtonSelector: '#slider-2-prev',
-    defaultActiveIndex: 0,
-    activeButtonClass: 'product-slider-active-btn',
-    activeButtonClassTarget: '.slide-2-btn',
-    ...config,
-  })
-
-  const mobileSlider3 = new Slider({
-    container: '#slider-container-3',
-    slideSelector: '.slide-3',
-    buttonSelector: '.slide-3-btn',
-    nextButtonSelector: '#slider-3-next',
-    prevButtonSelector: '#slider-3-prev',
-    defaultActiveIndex: 0,
-    activeButtonClass: 'product-slider-active-btn',
-    activeButtonClassTarget: '.slide-3-btn',
-    ...config,
-  })
-
-  const mobileSlider4 = new Slider({
-    container: '#slider-container-4',
-    slideSelector: '.slide-4',
-    buttonSelector: '.slide-4-btn',
-    nextButtonSelector: '#slider-4-next',
-    prevButtonSelector: '#slider-4-prev',
-    defaultActiveIndex: 0,
-    activeButtonClass: 'product-slider-active-btn',
-    activeButtonClassTarget: '.slide-4-btn',
-    ...config,
-  })
-
-  const multiGroupGallery = new MultiGroupImageGallery({
-    elements: {
-      modalId: 'multi-gallery',
-      modalContentId: 'multi-gallery-content',
-      mainImageContainerId: 'multi-gallery-main-image-container',
-      thumbnailsContainerId: 'multi-gallery-thumbnails',
-      prevButtonId: 'prev-multi-gallery',
-      nextButtonId: 'next-multi-gallery',
-      closeButtonId: 'close-multi-gallery',
-    },
-    groups: [
-      {
-        containerId: 'product-slider',
-        slideItemClass: 'product-slide',
-      },
-      {
-        containerId: 'slider-1-desktop',
-        slideItemClass: 'slide-1-desktop',
-      },
-      {
-        containerId: 'slider-2-desktop',
-        slideItemClass: 'slide-2-desktop',
-      },
-      {
-        containerId: 'slider-3-desktop',
-        slideItemClass: 'slide-3-desktop',
-      },
-      {
-        containerId: 'slider-4-desktop',
-        slideItemClass: 'slide-4-desktop',
-      },
-      {
-        containerId: 'slider-1',
-        slideItemClass: 'slide-1',
-      },
-      {
-        containerId: 'slider-2',
-        slideItemClass: 'slide-2',
-      },
-      {
-        containerId: 'slider-3',
-        slideItemClass: 'slide-3',
-      },
-      {
-        containerId: 'slider-4',
-        slideItemClass: 'slide-4',
-      },
-    ],
-    thumbnailClass: 'thumbnail',
-    activeThumbnailClass: 'thumbnail-active',
-  })
-
-  // Slider touch event detector
-  new TouchDirectionDetector('product-slider-container', {
-    threshold: 50,
-    onSwipe: direction => {
-      if (direction === 'right') {
-        return mainSlider.prev()
-      }
-      if (direction === 'left') {
-        return mainSlider.next()
-      }
-    },
-  })
-
-  new TouchDirectionDetector('multi-gallery-main-image-container', {
-    threshold: 50,
-    onSwipe: direction => {
-      if (direction === 'right') {
-        return multiGroupGallery.navigateGallery('prev')
-      }
-      if (direction === 'left') {
-        return multiGroupGallery.navigateGallery('next')
-      }
-    },
-  })
-
-  new TouchDirectionDetector('slider-container-1', {
-    threshold: 50,
-    onSwipe: direction => {
-      if (direction === 'right') {
-        return mobileSlider1.prev()
-      }
-      if (direction === 'left') {
-        return mobileSlider1.next()
-      }
-    },
-  })
-
-  new TouchDirectionDetector('slider-container-2', {
-    threshold: 50,
-    onSwipe: direction => {
-      if (direction === 'right') {
-        return mobileSlider2.prev()
-      }
-      if (direction === 'left') {
-        return mobileSlider2.next()
-      }
-    },
-  })
-
-  new TouchDirectionDetector('slider-container-3', {
-    threshold: 50,
-    onSwipe: direction => {
-      if (direction === 'right') {
-        return mobileSlider3.prev()
-      }
-      if (direction === 'left') {
-        return mobileSlider3.next()
-      }
-    },
-  })
-
-  new TouchDirectionDetector('slider-container-4', {
-    threshold: 50,
-    onSwipe: direction => {
-      if (direction === 'right') {
-        return mobileSlider4.prev()
-      }
-      if (direction === 'left') {
-        return mobileSlider4.next()
-      }
-    },
-  })
-})
-
+// Görseldeki ürünlerin sayısını otomatik olarak hesaplar ve slider'ın data-items değerini günceller
 document.addEventListener('DOMContentLoaded', () => {
   const productSliderElement = document.getElementById('product-slider')
   const childCount = productSliderElement
@@ -257,6 +21,400 @@ document.addEventListener('DOMContentLoaded', () => {
     'product-slider-image-count',
     childCount.toString(),
   )
+})
+
+// Main slider
+document.addEventListener('DOMContentLoaded', () => {
+  const mainSlider = new Slider({
+    container: '#product-slider-container',
+    slideSelector: '.product-slide',
+    buttonSelector: '.product-slider-btn',
+    nextButtonSelector: '#slider-next',
+    prevButtonSelector: '#slider-prev',
+    defaultActiveIndex: 0,
+    activeButtonClass: 'product-slider-active-btn',
+    activeButtonClassTarget: '.product-slider-btn-item',
+    ...mainConfig,
+    ...config,
+  })
+
+  new TouchDirectionDetector('product-slider-container', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return mainSlider.prev()
+      }
+      if (direction === 'left') {
+        return mainSlider.next()
+      }
+    },
+  })
+
+  const multiGroupGallery = new MultiGroupImageGallery({
+    groups: [
+      {
+        containerId: 'product-slider',
+        slideItemClass: 'product-slide',
+      },
+    ],
+    ...galleryConfig,
+  })
+
+  new TouchDirectionDetector('multi-gallery-main-image-container', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return multiGroupGallery.navigateGallery('prev')
+      }
+      if (direction === 'left') {
+        return multiGroupGallery.navigateGallery('next')
+      }
+    },
+  })
+})
+
+// Desktop Silver Package Slider
+document.addEventListener('DOMContentLoaded', () => {
+  new Slider({
+    container: '#slider-container-1-desktop',
+    slideSelector: '.slide-1-desktop',
+    buttonSelector: '.slide-1-btn-desktop',
+    nextButtonSelector: '#slider-1-next-desktop',
+    prevButtonSelector: '#slider-1-prev-desktop',
+    defaultActiveIndex: 0,
+    activeButtonClass: 'product-slider-active-btn',
+    activeButtonClassTarget: '.slide-1-btn-desktop',
+    ...config,
+  })
+
+  const multiGroupGallery = new MultiGroupImageGallery({
+    groups: [
+      {
+        containerId: 'slider-1-desktop',
+        slideItemClass: 'slide-1-desktop',
+      },
+    ],
+    ...galleryConfig,
+  })
+
+  new TouchDirectionDetector('multi-gallery-main-image-container', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return multiGroupGallery.navigateGallery('prev')
+      }
+      if (direction === 'left') {
+        return multiGroupGallery.navigateGallery('next')
+      }
+    },
+  })
+})
+
+// Desktop Gold Package Slider
+document.addEventListener('DOMContentLoaded', () => {
+  new Slider({
+    container: '#slider-container-2-desktop',
+    slideSelector: '.slide-2-desktop',
+    buttonSelector: '.slide-2-btn-desktop',
+    nextButtonSelector: '#slider-2-next-desktop',
+    prevButtonSelector: '#slider-2-prev-desktop',
+    defaultActiveIndex: 0,
+    activeButtonClass: 'product-slider-active-btn',
+    activeButtonClassTarget: '.slide-2-btn-desktop',
+    ...config,
+  })
+
+  const multiGroupGallery = new MultiGroupImageGallery({
+    groups: [
+      {
+        containerId: 'slider-2-desktop',
+        slideItemClass: 'slide-2-desktop',
+      },
+    ],
+    ...galleryConfig,
+  })
+
+  new TouchDirectionDetector('multi-gallery-main-image-container', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return multiGroupGallery.navigateGallery('prev')
+      }
+      if (direction === 'left') {
+        return multiGroupGallery.navigateGallery('next')
+      }
+    },
+  })
+})
+
+// Desktop Platinum Package Slider
+document.addEventListener('DOMContentLoaded', () => {
+  new Slider({
+    container: '#slider-container-3-desktop',
+    slideSelector: '.slide-3-desktop',
+    buttonSelector: '.slide-3-btn-desktop',
+    nextButtonSelector: '#slider-3-next-desktop',
+    prevButtonSelector: '#slider-3-prev-desktop',
+    defaultActiveIndex: 0,
+    activeButtonClass: 'product-slider-active-btn',
+    activeButtonClassTarget: '.slide-3-btn-desktop',
+    ...config,
+  })
+
+  const multiGroupGallery = new MultiGroupImageGallery({
+    groups: [
+      {
+        containerId: 'slider-3-desktop',
+        slideItemClass: 'slide-3-desktop',
+      },
+    ],
+    ...galleryConfig,
+  })
+
+  new TouchDirectionDetector('multi-gallery-main-image-container', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return multiGroupGallery.navigateGallery('prev')
+      }
+      if (direction === 'left') {
+        return multiGroupGallery.navigateGallery('next')
+      }
+    },
+  })
+})
+
+// Desktop Diamond Package Slider
+document.addEventListener('DOMContentLoaded', () => {
+  new Slider({
+    container: '#slider-container-4-desktop',
+    slideSelector: '.slide-4-desktop',
+    buttonSelector: '.slide-4-btn-desktop',
+    nextButtonSelector: '#slider-4-next-desktop',
+    prevButtonSelector: '#slider-4-prev-desktop',
+    defaultActiveIndex: 0,
+    activeButtonClass: 'product-slider-active-btn',
+    activeButtonClassTarget: '.slide-4-btn-desktop',
+    ...config,
+  })
+
+  const multiGroupGallery = new MultiGroupImageGallery({
+    groups: [
+      {
+        containerId: 'slider-4-desktop',
+        slideItemClass: 'slide-4-desktop',
+      },
+    ],
+    ...galleryConfig,
+  })
+
+  new TouchDirectionDetector('multi-gallery-main-image-container', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return multiGroupGallery.navigateGallery('prev')
+      }
+      if (direction === 'left') {
+        return multiGroupGallery.navigateGallery('next')
+      }
+    },
+  })
+})
+
+// Mobile Silver Package Slider
+document.addEventListener('DOMContentLoaded', () => {
+  const mobileSlider1 = new Slider({
+    container: '#slider-container-1',
+    slideSelector: '.slide-1',
+    buttonSelector: '.slide-1-btn',
+    nextButtonSelector: '#slider-1-next',
+    prevButtonSelector: '#slider-1-prev',
+    defaultActiveIndex: 0,
+    activeButtonClass: 'product-slider-active-btn',
+    activeButtonClassTarget: '.slide-1-btn',
+    ...config,
+  })
+
+  new TouchDirectionDetector('slider-container-1', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return mobileSlider1.prev()
+      }
+      if (direction === 'left') {
+        return mobileSlider1.next()
+      }
+    },
+  })
+
+  const multiGroupGallery = new MultiGroupImageGallery({
+    groups: [
+      {
+        containerId: 'slider-1',
+        slideItemClass: 'slide-1',
+      },
+    ],
+    ...galleryConfig,
+  })
+
+  new TouchDirectionDetector('multi-gallery-main-image-container', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return multiGroupGallery.navigateGallery('prev')
+      }
+      if (direction === 'left') {
+        return multiGroupGallery.navigateGallery('next')
+      }
+    },
+  })
+})
+
+// Mobile Gold Package Slider
+document.addEventListener('DOMContentLoaded', () => {
+  const mobileSlider2 = new Slider({
+    container: '#slider-container-2',
+    slideSelector: '.slide-2',
+    buttonSelector: '.slide-2-btn',
+    nextButtonSelector: '#slider-2-next',
+    prevButtonSelector: '#slider-2-prev',
+    defaultActiveIndex: 0,
+    activeButtonClass: 'product-slider-active-btn',
+    activeButtonClassTarget: '.slide-2-btn',
+    ...config,
+  })
+
+  new TouchDirectionDetector('slider-container-2', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return mobileSlider2.prev()
+      }
+      if (direction === 'left') {
+        return mobileSlider2.next()
+      }
+    },
+  })
+
+  const multiGroupGallery = new MultiGroupImageGallery({
+    groups: [
+      {
+        containerId: 'slider-2',
+        slideItemClass: 'slide-2',
+      },
+    ],
+    ...galleryConfig,
+  })
+
+  new TouchDirectionDetector('multi-gallery-main-image-container', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return multiGroupGallery.navigateGallery('prev')
+      }
+      if (direction === 'left') {
+        return multiGroupGallery.navigateGallery('next')
+      }
+    },
+  })
+})
+
+// Mobile Platinum Package Slider
+document.addEventListener('DOMContentLoaded', () => {
+  const mobileSlider3 = new Slider({
+    container: '#slider-container-3',
+    slideSelector: '.slide-3',
+    buttonSelector: '.slide-3-btn',
+    nextButtonSelector: '#slider-3-next',
+    prevButtonSelector: '#slider-3-prev',
+    defaultActiveIndex: 0,
+    activeButtonClass: 'product-slider-active-btn',
+    activeButtonClassTarget: '.slide-3-btn',
+    ...config,
+  })
+
+  new TouchDirectionDetector('slider-container-3', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return mobileSlider3.prev()
+      }
+      if (direction === 'left') {
+        return mobileSlider3.next()
+      }
+    },
+  })
+
+  const multiGroupGallery = new MultiGroupImageGallery({
+    groups: [
+      {
+        containerId: 'slider-3',
+        slideItemClass: 'slide-3',
+      },
+    ],
+    ...galleryConfig,
+  })
+
+  new TouchDirectionDetector('multi-gallery-main-image-container', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return multiGroupGallery.navigateGallery('prev')
+      }
+      if (direction === 'left') {
+        return multiGroupGallery.navigateGallery('next')
+      }
+    },
+  })
+})
+
+// Mobile Diamond Package Slider
+document.addEventListener('DOMContentLoaded', () => {
+  const mobileSlider4 = new Slider({
+    container: '#slider-container-4',
+    slideSelector: '.slide-4',
+    buttonSelector: '.slide-4-btn',
+    nextButtonSelector: '#slider-4-next',
+    prevButtonSelector: '#slider-4-prev',
+    defaultActiveIndex: 0,
+    activeButtonClass: 'product-slider-active-btn',
+    activeButtonClassTarget: '.slide-4-btn',
+    ...config,
+  })
+
+  new TouchDirectionDetector('slider-container-4', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return mobileSlider4.prev()
+      }
+      if (direction === 'left') {
+        return mobileSlider4.next()
+      }
+    },
+  })
+
+  const multiGroupGallery = new MultiGroupImageGallery({
+    groups: [
+      {
+        containerId: 'slider-4',
+        slideItemClass: 'slide-4',
+      },
+    ],
+    ...galleryConfig,
+  })
+
+  new TouchDirectionDetector('multi-gallery-main-image-container', {
+    threshold: 50,
+    onSwipe: direction => {
+      if (direction === 'right') {
+        return multiGroupGallery.navigateGallery('prev')
+      }
+      if (direction === 'left') {
+        return multiGroupGallery.navigateGallery('next')
+      }
+    },
+  })
 })
 
 const config = {
@@ -295,4 +453,18 @@ const mainConfig = {
     minWidth: 0,
     maxWidth: 1024,
   },
+}
+
+const galleryConfig = {
+  elements: {
+    modalId: 'multi-gallery',
+    modalContentId: 'multi-gallery-content',
+    mainImageContainerId: 'multi-gallery-main-image-container',
+    thumbnailsContainerId: 'multi-gallery-thumbnails',
+    prevButtonId: 'prev-multi-gallery',
+    nextButtonId: 'next-multi-gallery',
+    closeButtonId: 'close-multi-gallery',
+  },
+  thumbnailClass: 'thumbnail',
+  activeThumbnailClass: 'thumbnail-active',
 }
