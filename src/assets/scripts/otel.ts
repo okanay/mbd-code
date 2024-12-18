fee830e79c02385424b6900af53fc0e93ac89426
okanay <okanay@hotmail.com>
2024-12-18 23:07:48 +0300

    488

--- src/assets/scripts/otel.ts
+++ src/assets/scripts/otel.ts
@@ -37,7 +37,19 @@ document.addEventListener('DOMContentLoaded', () => {
         },
       },
       scrollLock: {
-        enabled: false,
+        enabled: true,
+        styles: {
+          hidden: {
+            overflow: 'hidden',
+            position: 'fixed',
+            width: '100%',
+          },
+          visible: {
+            overflow: 'auto',
+            position: 'static',
+            width: 'auto',
+          },
+        },
       },
     },
   )
